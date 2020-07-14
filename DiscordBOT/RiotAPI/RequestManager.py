6a43139dcbb45f6cab519298f4252139cbf76fc9
thiagokovnat <tkovnat@fi.uba.ar>
2020-07-14 00:21:18 -0300

    Added Discord Bot

---
+++ DiscordBOT/RiotAPI/RequestManager.py
@@ -0,0 +1,102 @@
+import json
+import cassiopeia as cass 
+from cassiopeia import Summoner, Champion
+import pandas as pd
+
+validRegions = ["LAN","LAS","BR","EUW","EUE"]
+
+class Jugador():
+
+	def __init__(self, name, league, tier, promo, LP, hotstreak):
+
+		self.name = name
+		self.league = league
+		self.promo = promo
+		self.LP = LP
+		self.tier = tier
+		self.hotstreak = hotstreak
+		self.championMasteries = []
+
+	def setChampionMasteries(self, champions):
+		self.championMasteries = [x.champion.name for x in champions]
+
+
+class PlayerTracker():
+
+	def __init__(self, region, key):
+
+		if region.upper() not in validRegions:
+			raise Exception("Invalid Region")
+
+		cass.set_riot_api_key(key)
+		cass.set_default_region(region)
+
+		self.region = region
+		
+	def setRegion(self, region):
+
+		if region.upper() not in validRegions:
+			raise Exception("Invalid Region")
+
+		else:
+			self.region = region
+
+	
+	def getSummoner(self, summonerName):
+
+		summoner = Summoner(name = summonerName, region = self.region)
+		
+		return summoner
+
+	def getRankedSummoner(self, summonerName):
+
+		summoner = self.getSummoner(summonerName)
+
+		entries = summoner.league_entries
+
+		if entries.fives.promos is not None:
+			return Jugador(summonerName, entries.fives.tier,entries.fives.division, True, entries.fives.league_points, entries.fives.hot_streak)
+		else:
+			return Jugador(summonerName, entries.fives.tier,entries.fives.division, False, entries.fives.league_points, entries.fives.hot_streak)
+
+	def getRankedFlexSummoner(self, summonerName):
+
+		summoner = self.getSummoner(summonerName)
+
+		entries = summoner.league_entries
+
+		if entries.fives.promos is not None:
+			return Jugador(summonerName, entries.flex.tier,entries.flex.division, True, entries.flex.league_points, entries.flex.hot_streak)
+		else:
+			
+			return Jugador(summonerName, entries.flex.tier,entries.flex.division, False, entries.flex.league_points, entries.flex.hot_streak)
+
+	def getChampion(self, championName):
+
+		champion = Champion(name = championName, region = self.region)
+
+		return champion
+
+	def getChampionMastery(self, summonerName):
+
+		summoner = self.getSummoner(summonerName)
+
+		master = summoner.champion_masteries
+
+		pro = master.filter(lambda cm: cm.level >= 5)
+
+		return pro
+
+	def RankedStats(self, summonerName, queue = "solo"):
+
+		if queue.lower() == "solo":
+			player = self.getRankedSummoner(summonerName)
+		else:
+			player = self.getRankedFlexSummoner(summonerName)
+
+		masteries = self.getChampionMastery(summonerName)
+
+		player.setChampionMasteries(masteries)
+
+		return player
+
---
+++ DiscordBOT/bot.py
@@ -0,0 +1,64 @@
+import asyncio
+import discord
+from discord.ext import commands
+import time
+from RiotAPI import RequestManager as RM
+
+
+client = commands.Bot(command_prefix = "!")
+token = "NzMxOTUxNjQ4Njg3MjU5NjY5.Xwtk-Q.dnZ0jGIyzrz20eDr9Mo-CsQSMZs"
+guild = "RSwrcb"
+riotapi = RM.PlayerTracker("LAS", "RGAPI-67e3b4c2-e609-44ea-820f-d7181d1db4f9")
+
+
+@client.event
+async def on_ready():
+	print("Succesfully connected to guild.")
+
+
+@client.command(name = "soloq", help = "gives information about a players soloq rank")
+async def getSoloQStats(ctx, summonerName):
+
+	try:
+		jugador = riotapi.getRankedSummoner(summonerName)
+
+		await ctx.send(f"""``` Name: {jugador.name.title()}.```""")
+		await ctx.send(f"""` Rank: {jugador.league} {jugador.tier}`""")
+		await ctx.send(f"""` LP: {jugador.LP}`""")
+		await ctx.send(f"""` Promo: {str(jugador.promo)}`""")
+		await ctx.send(f"""` Hot Streak: {str(jugador.hotstreak)}`""")
+		#await ctx.send(f"""` Top Champs: {jugador.championMasteries[0]}, {jugador.championMasteries[1]}, {jugador.championMasteries[2]}`""")
+   
+	except Exception as e:
+
+		await ctx.send("`No user found.`")
+
+@client.command(name = "region", help = "sets region for bot")
+async def setRegion(ctx, region):
+
+	try:
+
+		riotapi.setRegion(region)
+		await ctx.send(f"""```Region succesfully changed to: {region}```""")
+
+	except Exception as e:
+		await ctx.send("`Invalid Region`")
+
+@client.command(name = "flex", help = "gives ranked flex stats")
+async def getRankedFlex(ctx, summonerName):
+
+	try:
+		jugador = riotapi.getRankedFlexSummoner(summonerName)
+
+		await ctx.send(f"""``` Name: {jugador.name.title()}.```""")
+		await ctx.send(f"""` Rank: {jugador.league} {jugador.tier}`""")
+		await ctx.send(f"""` LP: {jugador.LP}`""")
+		await ctx.send(f"""` Promo: {str(jugador.promo)}`""")
+		await ctx.send(f"""` Hot Streak: {str(jugador.hotstreak)}`""")
+		#await ctx.send(f"""` Top Champs: {jugador.championMasteries[0]}, {jugador.championMasteries[1]}, {jugador.championMasteries[2]}`""")
+   
+	except Exception as e:
+
+		await ctx.send("`No user found.`")
+
+client.run(token)
